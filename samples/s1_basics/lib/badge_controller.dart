23735035e5168eb2371ebc260b3a28748f6616e1
AnimalRationale <wojciech.mardyla@gmail.com>
2015-01-26 23:51:26 +0100

    Added model class and bindings; random values in badge.

--- samples/s1_basics/lib/badge_controller.dart
+++ samples/s1_basics/lib/badge_controller.dart
@@ -1,5 +1,7 @@
 library s1_basics.badge_controller;
 
+import 'dart:math' show Random;
+import 'package:s1_basics/model.dart';
 import 'package:angular/angular.dart';
 
 @NgController(
@@ -7,15 +9,38 @@ import 'package:angular/angular.dart';
   publishAs: 'ctrl')
 
 class BadgeController {
-  static const DEFAULT_NAME = 'Anne Bonney';
     static const LABEL1 = 'Arrr! Write yer name!';
     static const LABEL2 = 'Aye! Gimme a name!';
-  String name = '';
+    static const List names = const [
+        'Anne', 'Mary', 'Jack', 'Morgan', 'Roger',
+        'Bill', 'Ragnar', 'Ed', 'John', 'Jane' ];
+
+      static const List appellations = const [
+        'Jackal', 'King', 'Red', 'Stalwart', 'Axe',
+        'Young', 'Brave', 'Eager', 'Wily', 'Zesty'];
+
+  PirateName pn = new PirateName();
+
+  String get pirateName => pn.firstName.isEmpty ? '' :
+        '${pn.firstName} the ${pn.appellation}';
 
   bool get inputIsNotEmpty => name.trim().isNotEmpty;
     String get label => inputIsNotEmpty ? LABEL1 : LABEL2;
 
-    generateName() {
-      name = DEFAULT_NAME;
+    String _oneRandom(List<String> list) =>
+             list[new Random().nextInt(list.length)];
+
+    void generateName() {
+      name = _oneRandom(names);
+    }
+
+    String _name = '';
+
+    get name => _name;
+
+    set name(newName) {
+      _name = newName;
+      pn..firstName = newName
+        ..appellation = _oneRandom(appellations);
     }
 }
