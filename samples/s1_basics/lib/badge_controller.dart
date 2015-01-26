bf149f6b46300f839269ed9d4a12cfa4d4d091c7
AnimalRationale <wojciech.mardyla@gmail.com>
2015-01-27 00:45:43 +0100

    Logic for reading data from JSON file.

--- samples/s1_basics/lib/badge_controller.dart
+++ samples/s1_basics/lib/badge_controller.dart
@@ -3,6 +3,7 @@ library s1_basics.badge_controller;
 import 'dart:math' show Random;
 import 'package:s1_basics/model.dart';
 import 'package:angular/angular.dart';
+import 'dart:async' show Future;
 
 @NgController(
   selector: '[badge-controller]',
@@ -11,15 +12,28 @@ import 'package:angular/angular.dart';
 class BadgeController {
     static const LABEL1 = 'Arrr! Write yer name!';
     static const LABEL2 = 'Aye! Gimme a name!';
-    static const List names = const [
-        'Anne', 'Mary', 'Jack', 'Morgan', 'Roger',
-        'Bill', 'Ragnar', 'Ed', 'John', 'Jane' ];
-
-      static const List appellations = const [
-        'Jackal', 'King', 'Red', 'Stalwart', 'Axe',
-        'Young', 'Brave', 'Eager', 'Wily', 'Zesty'];
-
+    static List<String> names = [];
+      static List<String> appellations = [];
+      final Http _http;
   PirateName pn = new PirateName();
+
+  bool dataLoaded = false;
+
+  BadgeController(this._http) {
+      _loadData()
+          .then((_) => dataLoaded = true)
+          .catchError((error) {
+            print('Could not read data from the JSON file: $error');
+          });
+    }
+
+  Future _loadData() {
+            return _http.get('packages/s1_basics/assets/piratenames.json')
+                .then((response) {
+                  names = response.data['names'];
+                  appellations = response.data['appellations'];
+                });
+          }
 
   String get pirateName => pn.firstName.isEmpty ? '' :
         '${pn.firstName} the ${pn.appellation}';
