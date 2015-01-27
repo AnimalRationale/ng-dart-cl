c3ed87d59fa7f0e95852a38fc3cf075b57c5d84b
AnimalRationale <wojciech.mardyla@gmail.com>
2015-01-27 22:45:09 +0100

    Service logic added to controller.

--- samples/s1_basics/lib/badge_controller.dart
+++ samples/s1_basics/lib/badge_controller.dart
@@ -1,62 +1,40 @@
 library s1_basics.badge_controller;
 
-import 'dart:math' show Random;
 import 'package:s1_basics/model.dart';
 import 'package:angular/angular.dart';
 import 'dart:async' show Future;
+import 'package:s1_basics/services/names_service.dart';
 
 @NgController(
-  selector: '[badge-controller]',
-  publishAs: 'ctrl')
-
-
+    selector: '[badge-controller]',
+    publishAs: 'ctrl')
 class BadgeController {
-  static const LABEL1 = 'Arrr! Write yer name!';
-  static const LABEL2 = 'Aye! Gimme a name!';
-  static List<String> names = [];
-  static List<String> appellations = [];
-  final Http _http;
+  NamesService ns;
 
   PirateName pn = new PirateName();
 
-  bool dataLoaded = false;
-
-  BadgeController(this._http) {
-      _loadData()
-          .then((_) => dataLoaded = true)
-          .catchError((error) {
-            print('Could not read data from the JSON file: $error');
-          });
-    }
-
-  Future _loadData() {
-            return _http.get('packages/s1_basics/assets/piratenames.json')
-                .then((response) {
-                  names = response.data['names'];
-                  appellations = response.data['appellations'];
-                });
-          }
-
   String get pirateName => pn.firstName.isEmpty ? '' :
-        '${pn.firstName} the ${pn.appellation}';
+    '${pn.firstName} the ${pn.appellation}';
+
+  BadgeController(this.ns);
+
+  String _name = '';
+
+  get name => _name;
+
+  set name(newName) {
+    _name = newName;
+    ns.randomAppellation().then((appellation) {
+      pn..firstName = newName
+        ..appellation = appellation;
+    });
+  }
 
   bool get inputIsNotEmpty => name.trim().isNotEmpty;
-    String get label => inputIsNotEmpty ? LABEL1 : LABEL2;
+  String get label => inputIsNotEmpty ? "Arrr! Write yer name!" :
+    "Aye! Gimme a name!";
 
-    String _oneRandom(List<String> list) =>
-             list[new Random().nextInt(list.length)];
-
-    void generateName() {
-      name = _oneRandom(names);
-    }
-
-    String _name = '';
-
-    get name => _name;
-
-    set name(newName) {
-      _name = newName;
-      pn..firstName = newName
-        ..appellation = _oneRandom(appellations);
-    }
+  Future generateName() => ns.randomName().then((aName) {
+    name = aName;
+  });
 }
